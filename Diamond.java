44e99038518007445194ea9da301382401abd728
NISHA27SHARMA <47317944+NISHA27SHARMA@users.noreply.github.com>
2019-02-13 08:17:49 -0800

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,34 @@
+//Nested Loop
+
+class Diamond
+{
+public static void main(String[] args)
+{
+int n=7;
+for(int i=0;i<=n;i++)
+{
+for(int j=1;j<=n-i;j++)
+{
+System.out.print(" ");
+}
+for(int k=1;k<=i;k++)
+{
+System.out.print("* ");
+}
+System.out.println("");
+}
+for(int i=1;i<=n-1;i++)
+{
+for(int j=1;j<=i;j++)
+{				
+System.out.print(" ");
+}
+for(int k=1;k<=n-i;k++)
+{
+System.out.print("* ");
+}
+System.out.println("");
+}
+}
+}
+
